e1f372349706d62520107acf3ddee6efa557b74f
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2023-05-12 16:48:02 +0700

    feat: add flame sensor and gas sensor

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #define ADC_VREF_mV 5000.0
 #define ADC_RESOLUTION 4095.0
 #define LM35_SENSOR 36
+#define FLAME_SENSOR 4
+#define GAS_SENSOR 15
 
 #define RED_LED 12
 #define GREEN_LED 13
@@ -15,17 +17,36 @@ void setup()
   pinMode(GREEN_LED, OUTPUT);
   pinMode(RED_LED, OUTPUT);
   pinMode(BUZZER, OUTPUT);
+
+  pinMode(LM35_SENSOR, INPUT);
+  pinMode(FLAME_SENSOR, INPUT);
+  pinMode(GAS_SENSOR, INPUT);
 }
 
 void loop()
 {
-  int adcVal = analogRead(LM35_SENSOR);
-  float milliVolt = adcVal * (ADC_VREF_mV / ADC_RESOLUTION);
-  float temp = milliVolt / 10;
+  int tempRaw = analogRead(LM35_SENSOR);
+  float tempMilliVolt = tempRaw * (ADC_VREF_mV / ADC_RESOLUTION);
+  float temp = tempMilliVolt / 10;
+
+  int gasRaw = analogRead(GAS_SENSOR);
+
+  int flameRaw = digitalRead(FLAME_SENSOR);
 
   Serial.print("Temperature: ");
   Serial.print(temp);
-  Serial.println("°C");
+  Serial.print("°C");
+  Serial.print(" ~ ");
+  Serial.print("Gas: ");
+  Serial.print(gasRaw);
+  Serial.print("ppm");
+  Serial.print(" ~ ");
+
+  if (flameRaw == HIGH) {
+    Serial.println("ada api");
+  } else {
+    Serial.println("tidak ada api");
+  }
 
   delay(1000);
 }
