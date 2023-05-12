f688bff0ed2949560d73d9915f66a4388cafb096
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2023-05-12 13:05:24 +0700

    feat: read input DHT22

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,40 @@
 #include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+
+#define DHT_SENSOR_PIN 15
+#define DHT_SENSOR_TYPE DHT11
 
 #define RED_LED 12
 #define GREEN_LED 13
 #define BUZZER 14
+
+DHT dht_sensor(DHT_SENSOR_PIN, DHT_SENSOR_TYPE);
 
 void setup() {
   Serial.begin(115200);
   pinMode(GREEN_LED, OUTPUT);
   pinMode(RED_LED, OUTPUT);
   pinMode(BUZZER, OUTPUT);
+
+  dht_sensor.begin();
 }
 
 void loop() {
-  digitalWrite(GREEN_LED, HIGH);
-  digitalWrite(RED_LED, HIGH);
-  digitalWrite(BUZZER, HIGH);
-  delay(300);
-  digitalWrite(GREEN_LED, LOW);
-  digitalWrite(RED_LED, LOW);
-  digitalWrite(BUZZER, LOW);
-  delay(300);
+  float humi  = dht_sensor.readHumidity();
+  float temp = dht_sensor.readTemperature();
+
+  if ( isnan(temp) || isnan(humi)) {
+    Serial.println("Failed to read from DHT sensor!");
+  } else {
+    Serial.print("Humidity: ");
+    Serial.print(humi);
+    Serial.print("%");
+    Serial.print("  |  ");
+    Serial.print("Temperature: ");
+    Serial.print(temp);
+    Serial.println("°C");
+  }
+
+  delay(2000);
 }
