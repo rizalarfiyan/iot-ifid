1dc35eabb8b441c9ffeff984dd5ea62adf78dd35
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2023-05-16 07:39:33 +0700

    feat: add wifi connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <WiFi.h>
 #include <LiquidCrystal_I2C.h>
 #include <ShiftRegister74HC595.h>
 
@@ -6,7 +7,7 @@
 #define ADC_RESOLUTION 4095.0
 #define LM35_SENSOR 36
 #define FLAME_SENSOR 4
-#define GAS_SENSOR 15
+#define GAS_SENSOR 34
 
 #define RED_LED 12
 #define GREEN_LED 13
@@ -15,6 +16,9 @@
 #define DATA_PIN 25
 #define LATCH_PIN 33
 #define CLOCK_PIN 32
+
+const char* ssid = "iot";
+const char* password = "1234567890";
 
 ShiftRegister74HC595<1> sr(DATA_PIN, CLOCK_PIN, LATCH_PIN);
 
@@ -34,6 +38,24 @@ void setup()
 
   lcd.init();
   lcd.backlight();
+
+  bool ledOn = false;
+  digitalWrite(GREEN_LED, HIGH);
+  delay(300);
+
+  Serial.printf("\n\nConnecting to %s \n", ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(250);
+    Serial.print(".");
+    digitalWrite(GREEN_LED, ledOn);
+    ledOn = !ledOn;
+  }
+
+  Serial.println("\nWiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  digitalWrite(GREEN_LED, HIGH);
 }
 
 void loop()
@@ -55,7 +77,7 @@ void loop()
   Serial.print("ppm");
   Serial.print(" ~ ");
 
-  if (flameRaw == HIGH) {
+  if (flameRaw != HIGH) {
     Serial.println("ada api");
   } else {
     Serial.println("tidak ada api");
@@ -66,7 +88,7 @@ void loop()
   lcd.print((char)223);
   lcd.print("C");
   lcd.setCursor(0, 1);
-  lcd.printf("Gas : %.2f ppm", gasRaw);
+  lcd.printf("Gas : %d ppm", gasRaw);
 
   delay(1000);
 }
