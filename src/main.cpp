2fd057d67314b8d061764af379a6fba1e48837d0
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2023-07-12 01:55:00 +0700

    feat: finishing logic and code

--- src/main.cpp
+++ src/main.cpp
@@ -43,14 +43,28 @@ WidgetLCD blynkLcd(V2);
 float temp = 0.0;
 int gas = 0;
 bool flameStatus = false;
+DynamicJsonDocument data(1024);
+
 bool isAlarm = false;
 bool stateNotification = false;
-DynamicJsonDocument data(1024);
+
+// 0 => aman
+// 1 => gas naik
+// 2 => suhu naik
+// 3 => ada api
+// 4 => gas dan suhu naik
+// 5 => gas dan suhu naik, ada api
+int status, prevStatus = 0;
 
 void triggerAction();
 void triggerWebhook();
 void notificationAction();
 void readSensors();
+void setStatus(int state);
+bool isSameStatus();
+void handleAction();
+void setBlynkLCD(const char statusChar[]);
+void handleTriggerAction(const uint8_t valueBlower, const uint8_t valueSprinkler);
 
 void setup()
 {
@@ -71,6 +85,8 @@ void setup()
   digitalWrite(GREEN_LED, HIGH);
   delay(300);
 
+  lcd.setCursor(0, 0);
+  lcd.print("Connecting...");
   Serial.printf("\n\nConnecting to %s \n", ssid);
   WiFi.begin(ssid, pass);
   while (WiFi.status() != WL_CONNECTED) {
@@ -80,6 +96,11 @@ void setup()
     ledOn = !ledOn;
   }
 
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Connected!");
+  lcd.setCursor(0, 1);
+  lcd.print(WiFi.localIP());
   Serial.println("\nWiFi connected");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
@@ -88,6 +109,7 @@ void setup()
   blynkLcd.clear();
   blynkLcd.print(0, 0, "IP address:");
   blynkLcd.print(0, 1, WiFi.localIP());
+  delay(500);
 
   Blynk.begin(auth, ssid, pass, server, port);
   sensorDelay.start(SENSOR_DELAY);
@@ -102,56 +124,121 @@ void loop()
   readSensors();
 }
 
+void setStatus(int state) {
+  prevStatus = status;
+  status = state;
+}
+
 void triggerAction() {
   bool highTemp = temp >= 80;
-  bool highGas = gas > 6000;
-  if (highGas) {
-    data["status"] = "gas menaik";
-    Serial.println("Gas Menaik");
-    isAlarm = false;
-    sr.set(0, HIGH);
-    sr.set(1, LOW);
-    blynkLcd.clear();
-    blynkLcd.print(0, 0, "Status:");
-    blynkLcd.print(0, 1, "Gas Naik");
-    triggerWebhook();
-  } else if (highTemp) {
-    data["status"] = "suhu menaik";
-    Serial.println("Suhu menaik");
-    isAlarm = false;
-    sr.set(0, HIGH);
-    sr.set(1, LOW);
-    blynkLcd.clear();
-    blynkLcd.print(0, 0, "Status:");
-    blynkLcd.print(0, 1, "Suhu Naik");
-    triggerWebhook();
+  bool highGas = gas > 4000;
+  if (highGas && highTemp && flameStatus) {
+    setStatus(5);
   } else if (highGas && highTemp) {
-    data["status"] = "gas dan suhu menaik";
-    Serial.println("Gas dan suhu menaik");
-    isAlarm = true;
-    sr.set(0, HIGH);
-    sr.set(1, LOW);
-    blynkLcd.clear();
-    blynkLcd.print(0, 0, "Status:");
-    blynkLcd.print(0, 1, "Gas Suhu Naik");
-    triggerWebhook();
-  } else if (highGas && highTemp && flameStatus) {
-    data["status"] = "Gas dan suhu menaik, ada api";
-    Serial.println("Gas dan suhu menaik, ada api");
-    isAlarm = true;
-    sr.set(0, HIGH);
-    sr.set(1, HIGH);
-    blynkLcd.clear();
-    blynkLcd.print(0, 0, "Status:");
-    blynkLcd.print(0, 1, "Warning");
-    triggerWebhook();
+    setStatus(4);
+  } else if (flameStatus) {
+    setStatus(3);
+  } else if (highTemp) {
+    setStatus(2);
+  } else if (highGas) {
+    setStatus(1);
   } else {
-    blynkLcd.clear();
-    blynkLcd.print(0, 0, "Status:");
-    blynkLcd.print(0, 1, "Online");
+    setStatus(0);
+  }
+  handleAction();
+}
+
+void handleAction() {
+  if (status == prevStatus) return;
+
+  switch (status)
+  {
+  case 5:
+    isAlarm = true;
+    setBlynkLCD("Bahaya");
+    Serial.println("Gas dan suhu naik, ada api");
+    data["message"] = "Gas dan suhu naik, ada api";
+    data["state"] = 5;
+    triggerWebhook();
+    handleTriggerAction(1, 1);
+    break;
+  case 4:
+    isAlarm = true;
+    setBlynkLCD("Bahaya");
+    Serial.println("Gas dan suhu naik");
+    data["message"] = "Gas dan suhu naik";
+    data["state"] = 4;
+    triggerWebhook();
+    handleTriggerAction(1, 0);
+    break;
+  case 3:
+    isAlarm = true;
+    setBlynkLCD("Bahaya");
+    Serial.println("Ada api");
+    data["message"] = "Ada api";
+    data["state"] = 3;
+    triggerWebhook();
+    handleTriggerAction(1, 1);
+    break;
+  case 2:
+    isAlarm = true;
+    setBlynkLCD("Waspada");
+    Serial.println("Suhu naik");
+    data["message"] = "Suhu naik";
+    data["state"] = 2;
+    triggerWebhook();
+    handleTriggerAction(1, 0);
+    break;
+  case 1:
+    isAlarm = true;
+    setBlynkLCD("Waspada");
+    Serial.println("Gas naik");
+    data["message"] = "Gas naik";
+    data["state"] = 1;
+    triggerWebhook();
+    handleTriggerAction(1, 0);
+    break;
+  default:
     isAlarm = false;
-    sr.set(0, LOW);
-    sr.set(1, LOW);
+    setBlynkLCD("Online");
+    handleTriggerAction(0, 0);
+    break;
+  }
+}
+
+void setBlynkLCD(const char statusChar[]) {
+  blynkLcd.clear();
+  blynkLcd.print(0, 0, "Status:");
+  blynkLcd.print(0, 1, statusChar);
+}
+
+void handleTriggerAction(const uint8_t valueBlower, const uint8_t valueSprinkler) {
+  sr.set(0, valueBlower);
+  Blynk.virtualWrite(V3, valueBlower);
+  sr.set(1, valueSprinkler);
+  Blynk.virtualWrite(V4, valueSprinkler);
+}
+
+void notificationAction() {
+  if (notificationDelay.justFinished()) {
+    notificationDelay.repeat();
+
+    if (!isAlarm) {
+      digitalWrite(RED_LED, LOW);
+      digitalWrite(BUZZER, LOW);
+      return;
+    }
+
+    if (stateNotification) {
+      digitalWrite(RED_LED, HIGH);
+      digitalWrite(BUZZER, HIGH);
+      stateNotification = false;
+      return;
+    }
+
+    digitalWrite(RED_LED, LOW);
+    digitalWrite(BUZZER, LOW);
+    stateNotification = true;
   }
 }
 
@@ -159,24 +246,6 @@ void triggerWebhook() {
   char outputData[1024];
   serializeJson(data, outputData);
   Blynk.virtualWrite(V11, outputData);
-}
-
-void notificationAction() {
-  if (notificationDelay.justFinished()) {
-    notificationDelay.repeat();
-
-    if (isAlarm) {
-      if (stateNotification) {
-        digitalWrite(RED_LED, HIGH);
-        digitalWrite(BUZZER, HIGH);
-        stateNotification = false;
-      } else {
-        digitalWrite(RED_LED, LOW);
-        digitalWrite(BUZZER, LOW);
-        stateNotification = true;
-      }
-    }
-  }
 }
 
 void readSensors()
@@ -190,6 +259,7 @@ void readSensors()
 
     gas = analogRead(GAS_SENSOR);
     int flameRaw = digitalRead(FLAME_SENSOR);
+    flameStatus = flameRaw == HIGH;
 
     Serial.print("Temperature: ");
     Serial.print(temp);
@@ -199,14 +269,13 @@ void readSensors()
     Serial.print(gas);
     Serial.print("PPM");
     Serial.print(" ~ ");
-
-    flameStatus = flameRaw != HIGH;
     if (flameStatus) {
-      Serial.println("ada api");
+      Serial.print("ada api");
     } else {
-      Serial.println("tidak ada api");
+      Serial.print("tidak ada api");
     }
 
+    lcd.clear();
     lcd.setCursor(0, 0);
     lcd.printf("Temp: %.2f", temp);
     lcd.print((char)223);
@@ -216,9 +285,9 @@ void readSensors()
 
     Blynk.virtualWrite(V0, temp);
     Blynk.virtualWrite(V1, gas);
-    Blynk.virtualWrite(V2, flameRaw != HIGH);
+    Blynk.virtualWrite(V2, flameStatus);
 
-    data["flame"] = flameRaw != HIGH;
+    data["flame"] = flameStatus;
     data["temperature"] = temp;
     data["gas"] = gas;
   }
@@ -298,6 +367,6 @@ BLYNK_WRITE(V10)
 
 BLYNK_WRITE(V11)
 {
-  Serial.println("WebHook data:");
+  Serial.println("Webhook:");
   Serial.println(param.asStr());
 }
